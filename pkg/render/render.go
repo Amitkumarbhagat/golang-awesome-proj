b964f291063c1d0bc3caec12afbe04295e81f404
amit bhagat <amitbhgat2506@gmail.com>
2023-03-04 00:24:45 +0530

    completed simple not very complex building a template cache work but in next we will see complex template cache renderer

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 
 	err := parsedTemplate.Execute(w, nil)
@@ -20,7 +20,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 var tc = make(map[string]*template.Template)
 
-func RenderTemplateTest(w http.ResponseWriter, t string) {
+func RenderTemplate(w http.ResponseWriter, t string) {
 	var tmpl *template.Template
 	var err error
 
@@ -28,13 +28,21 @@ func RenderTemplateTest(w http.ResponseWriter, t string) {
 	_, inMap := tc[t]
 	if !inMap {
 		//	need to create the template
+		log.Println("cresting template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+
 	} else {
 		//	we have template in cache
 		log.Println("using cached template")
 	}
 	tmpl = tc[t]
-
 	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func createTemplateCache(t string) error {
@@ -44,5 +52,12 @@ func createTemplateCache(t string) error {
 	}
 
 	//	parse the template
-	tmpl, err := template.ParseFiles()
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	//	add template to cache (map)
+	tc[t] = tmpl
+	return nil
 }
