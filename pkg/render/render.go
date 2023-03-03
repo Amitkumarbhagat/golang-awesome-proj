a0f4ec470bbb69537da99f71123836fcaf5ae2ed
amit bhagat <amitbhgat2506@gmail.com>
2023-03-04 00:07:09 +0530

    building a simple template cache work on process

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,6 +3,7 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -15,4 +16,33 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		fmt.Println("error parsing template:", err)
 		return
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	//check to see if we already have the template in our cache
+	_, inMap := tc[t]
+	if !inMap {
+		//	need to create the template
+	} else {
+		//	we have template in cache
+		log.Println("using cached template")
+	}
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	//	parse the template
+	tmpl, err := template.ParseFiles()
 }
